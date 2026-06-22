e6522468b612a36be328e56c6f61b733b45b60a3
Amrudin Balić <amrudinbalic@192.168.1.6>
2026-06-22 19:31:23 +0200

    Adding basic pages

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 # mental map
-# request -> nginx
-# gunicorn - wsgi
-# app code
-# gunicorn - api
+# request -> nginx ->
+# -> gunicorn (input) -> app code (custom/django/flask) -> gunicorn (output)
 
-# WSGI interface contract
+# note: Gunicorn is built on top of WSGI definition
+
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
+def read_view(name):
+    return (BASE_DIR / "resources" / "views" / name).read_bytes()
+
+
+# WSGI interface contract - Gunicorn calls it -> boot the app code
 def application(environ, start_response):
     path = environ.get('PATH_INFO', '/')
+    request_method = environ['REQUEST_METHOD']
 
-    if path == '/':
-        body = b'Hello from home page!'
+    content_type = 'text/html'
+
+    if path == '/' and request_method == 'GET':
+        body = read_view('index.html')
         status = '200 OK'
+        content_type = 'text/html'
     elif path == '/about':
         body = b'About page'
         status = '200 OK'
@@ -19,8 +31,8 @@ def application(environ, start_response):
         status = '404 Not Found'
 
     headers = [
-        ('Content-Type', 'text/plain'),
-        ('Content-Length', str(len(body)))
+        ('Content-Type', content_type or 'text/html'), # -->> default
+        ('Content-Length', str( len(body) ))
     ]
     start_response(status, headers)
     return [body]
